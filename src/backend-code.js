6c7bfd593bcd9b58bdd8faede95a45f4ebfd38f0
saswati <p403saswati@gmail.com>
2019-07-22 08:27:14 -0700

    env file not working

--- src/backend-code.js
+++ src/backend-code.js
@@ -3,8 +3,9 @@ import 'jquery';
 export class DoctorSearch {
   getDoctorByName(firstName, lastName, doctorLocation) {
     return new Promise(function(resolve, reject) {
+      console.log(process.env.apiKey)
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName} &location=${doctorLocation}&user_key=202838dd5fcd4902f8cc192df156e601`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName} &location=${doctorLocation}&user_key=${process.env.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -20,7 +21,7 @@ export class DoctorSearch {
   getDoctorByQuery(query, doctorLocation) {
       return new Promise(function(resolve, reject) {
         let request = new XMLHttpRequest();
-        let url = `https://cors-anywhere.herokuapp.com/https://api.betterdoctor.com/2016-03-01/doctors?skip=0&limit=100&query=${query} &location=${doctorLocation}&user_key=202838dd5fcd4902f8cc192df156e601`;
+        let url = `https://cors-anywhere.herokuapp.com/https://api.betterdoctor.com/2016-03-01/doctors?skip=0&limit=100&query=${query} &location=${doctorLocation}&user_key=${process.env.apiKey}`;
         request.onload = function() {
           if (this.status === 200) {
             resolve(request.response);
